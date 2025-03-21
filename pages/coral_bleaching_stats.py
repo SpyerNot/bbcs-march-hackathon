2d4c842248b7b3dd313beb76594840535bceb8b6
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-03-21 16:07:51 +0800

    update

--- pages/coral_bleaching_stats.py
+++ pages/coral_bleaching_stats.py
@@ -3,3 +3,5 @@ st.image("coral.png",caption="Graph to show Coral Bleaching graph over the years
 st.write("This shows that coral bleaching is becoming more serious each year and it will reach 90% coral bleaching by 2050")
 st.write("Now you may ask: Eh, coral reefs is in the sea, not the land, is not gonna affect us. Rightttt????")
 st.write("Well the truth is.. NO!!!! Coral reefs provide fishes and other organisms shelter, find food, reproduce, and rear their young in the many nooks and crannies formed by corals. Without the corals providing all these to the organisms will we get our seafood?")
+st.write("Half a billion people rely on coral reefs for food and income. But reefs provide more than food. They also provide protection. Healthy reefs protect land from the damaging effects of tropical storms, shielding the shoreline from waves.")
+st.write("Soooo yes, corals very important. Have I convinced you yet?")
