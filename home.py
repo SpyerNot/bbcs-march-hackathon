ff3fd52936cdc95a9e1763ef49ba17e27a1e796b
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-03-21 16:05:07 +0800

    update

--- home.py
+++ home.py
@@ -1,1 +1,7 @@
 import streamlit as st
+st.header("Problem statement: Analyze the issue on coral bleaching and how does it affect us?")
+st.page_link("pages/coral_bleaching_stats.py", label="Coral Bleaching stats over the years", icon ="🪸")
+st.page_link("pages/conclusion.py",lable="Conclusion,")
+st.divider()
+st.caption("Made from group mar_52. Members are: Low Li Wen, Heng Zhi Xuan, Tay Jing Yee and Jin Ziyan")
+
---
+++ pages/coral_bleaching_stats.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.image("coral.png",caption="Graph to show Coral Bleaching graph over the years")
+st.write("This shows that coral bleaching is becoming more serious each year and it will reach 90% coral bleaching by 2050")
+st.write("Now you may ask: Eh, coral reefs is in the sea, not the land, is not gonna affect us. Rightttt????")
+st.write("Well the truth is.. NO!!!! Coral reefs provide fishes and other organisms shelter, find food, reproduce, and rear their young in the many nooks and crannies formed by corals. Without the corals providing all these to the organisms will we get our seafood?")
